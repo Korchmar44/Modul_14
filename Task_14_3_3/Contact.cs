dee689246bdbc5cef5be1cda6666b78890f150e9
Денис Голубев <d.golubev@corp.local>
2024-05-02 22:11:58 +0300

    Добавьте файлы проекта.

---
+++ Task_14_3_3/Contact.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_3_3
+{
+    public class Contact
+    {
+        public string Name { get; }
+        public string LastName { get; }
+        public string PhoneNumber { get; }
+        public string Email { get; }
+
+        public List<Contact> Contacts { get; private set; }
+
+        public Contact() { }
+
+        public Contact(string name, string lastName, string phoneNumber, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Contacts = new List<Contact>();
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Last Name: {LastName}, Phone Number: {PhoneNumber}, Email: {Email}";
+        }
+
+        public List<Contact> AddContact(Contact newContact)
+        {
+            if (Contacts == null)
+            {
+                Contacts = new List<Contact>();
+            }
+
+            if (!Contacts.Any() || !Contacts.Any(c => c.Name == newContact.Name && c.PhoneNumber == newContact.PhoneNumber))
+            {
+                Contacts.Add(newContact);
+            }
+
+            return Contacts;
+        }
+
+        public List <Contact> GetContactsByName() 
+        {
+            Contacts = (from c in Contacts orderby c.Name select c).ToList();
+            return Contacts;
+        }
+
+        public List<Contact> GetContactsByLastName()
+        {
+            Contacts = (from c in Contacts orderby c.LastName select c).ToList();
+            return Contacts;
+        }
+    }
+}
---
+++ Task_14_3_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_3_3
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact(); // создаем пустой объект Contact для доступа к методу AddContact
+
+            contact.AddContact(new Contact("Иван", "Иванов", "+123456789", "ivanov@example.com"));
+            contact.AddContact(new Contact("Петр", "Петров", "+987654321", "petrov@example.com"));
+            contact.AddContact(new Contact("Мария", "Сидорова", "+555555555", "sidorova@example.com"));
+            contact.AddContact(new Contact("Елена", "Козлова", "+333333333", "kozlova@example.com"));
+            contact.AddContact(new Contact("Алексей", "Смирнов", "+999999999", "smirnov@example.com"));
+
+            List<Contact> allContacts = contact.Contacts; // получаем обновленный список контактов
+
+            Console.WriteLine("Без сортировки");
+            foreach (var item in allContacts)
+            {
+                Console.WriteLine(item.ToString()); // Выводим информацию о контакте в консоль
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Сортировка по имени");
+            allContacts = contact.GetContactsByName();
+
+            foreach (var item in allContacts)
+            {
+                Console.WriteLine(item.ToString()); // Выводим информацию
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Сортировка по Фамилии");
+            allContacts = contact.GetContactsByLastName();
+
+            foreach (var item in allContacts)
+            {
+                Console.WriteLine(item.ToString()); // Выводим информацию
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
